78a6f512a577e8c815d7096cfc556c70f55d6f2b
valiantkui <528227496@qq.com>
2019-04-10 09:38:32 +0800

    first

---
+++ common/src/main/java/kui/common/entity/SohuResult.java
@@ -0,0 +1,18 @@
+package kui.common.entity;
+
+import java.util.List;
+
+public class SohuResult {
+
+	private List<SohuNBA> data;
+
+	public List<SohuNBA> getData() {
+		return data;
+	}
+
+	public void setData(List<SohuNBA> data) {
+		this.data = data;
+	}
+	
+	
+}
